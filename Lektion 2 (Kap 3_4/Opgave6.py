417e909241900746ee5f93b9ff97760018be3888
itchy83 <aj83@live.dk>
2019-02-12 20:53:29 +0100

    update

--- Lektion 2 (Kap 3_4/Opgave6.py
+++ Lektion 2 (Kap 3_4/Opgave6.py
@@ -1,10 +1,12 @@
 TalListe3 = list(range(3, 99+1, 3))
 print(TalListe3)
-AntalMin = min(TalListe3)       #min finder mindste tal i listen
+AntalMin = min(TalListe3)       # min finder mindste tal i listen
 print(AntalMin)
-AntalMax = max(TalListe3)       #max finder største tal i listen
+AntalMax = max(TalListe3)       # max finder største tal i listen
 print(AntalMax)
-Sum = sum(TalListe3)            #sum regner snittet ud
+Sum = sum(TalListe3)            # sum regner snittet ud
 print(Sum)
-Gennemsnit = sum(TalListe3)/len(TalListe3) #udregning af gennemsnittet i tallisten
+Gennemsnit = sum(TalListe3)/len(TalListe3) # udregning af gennemsnittet i tallisten
 print(Gennemsnit)
+
+# Hej Anel
