c567ff717379433598b9bc661760b71c1eb6dbf0
itchy83 <aj83@live.dk>
2019-02-12 20:40:39 +0100

    update

--- Lektion 1 (Kap. 1_2)/Opgave3.py
+++ Lektion 1 (Kap. 1_2)/Opgave3.py
@@ -1,10 +1,4 @@
 
-'Made By AJ'
-'Fælledvej 72'
-'8700 Horsens'
-'30454361'
-
-
 A = 7+5     # Variabel 1
 B = 36/4    # Variabel 2 Hvis man sætter 2 stk // ind viser det som int
 C = 3*4     # Variabel 3
